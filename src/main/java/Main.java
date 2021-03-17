fe76d41c97605a5e9cf05f4ac4684c3d69e10776
Rafael Reyes López <rafael.reyes.lopez@idbmobile.com>
2021-03-17 09:58:59 +0100

    avg days in service calculation

---
+++ src/main/java/Main.java
@@ -0,0 +1,74 @@
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+
+import scala.Tuple2;
+import scala.Tuple3;
+
+/**
+ *
+ * @author Rafael Reyes Lopez
+ * @email rafareyeslopez@gmail.com
+ * @date 2021-03-03
+ *
+ */
+public class Main {
+
+	public static void main(final String[] args) {
+
+		// Set logging level to skip WARN messages
+		Logger.getLogger("org.apache").setLevel(Level.WARN);
+
+		// Set Hadoop directory
+		System.setProperty("hadoop.home.dir", "c:/hadoop");
+
+		// Configuration for Spark context
+		final SparkConf conf = new SparkConf().setAppName("startingSpark").setMaster("local[*]");
+		final JavaSparkContext sc = new JavaSparkContext(conf);
+
+		// Load the data from database
+		final SparkSession spark = SparkSession.builder().appName("Spark calculation of average days subscribed")
+				.getOrCreate();
+
+		// Query database for the desired data, note the connection details
+		final Dataset<Row> load = spark.read().format("jdbc")
+				.option("url", "jdbc:mysql://database_ip:3306?user=username&password=password")
+				.option("query",
+						"SELECT user_id,subscription_timestamp,unsubscription_timestamp  FROM service.subscriptions")
+				.load();
+
+		// Convert the Dataset into a JavaRDD
+		final JavaRDD<Tuple3<String, Timestamp, Timestamp>> usersMap = load.toJavaRDD()
+				.map(row -> new Tuple3<>(row.getString(0), row.getTimestamp(1), row.getTimestamp(2)));
+
+		// Get for each user how many days has been into service
+		final JavaPairRDD<String, Long> mapToPair = usersMap.mapToPair(row -> new Tuple2<>(row._1(),
+				TimeUnit.MILLISECONDS.toDays(
+
+						row._3() == null ? Calendar.getInstance().getTimeInMillis() - row._2().getTime()
+								: row._3().getTime() - row._2().getTime()
+
+				)));
+
+		// Get the total of users for calculate average
+		final long count = mapToPair.count();
+
+		// Get the sum of days in service
+		final Long reduce = mapToPair.values().reduce((value1, value2) -> value1 + value2);
+
+		// Print out the average
+		System.out.println("Average days subscribed " + reduce / count);
+
+	}
+
+}
